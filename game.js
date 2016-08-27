1f1a64f8ec61bbc9d8ee16ffafce2311072bcc1a
bizkanta <totivan.aliz@gmail.com>
2016-08-27 12:14:54 +0200

    create basic board, without functionality

--- game.js
+++ game.js
@@ -2,5 +2,11 @@
 
 angular.module('ticTacToe', [])
   .controller('GameCtrl', ['$scope', function($scope) {
-    $scope.world = 'World!';
+    var player = 'x';
+    var player2 = 'o';
+    $scope.board = [
+      [null, player2, player],
+      [player2, null, player],
+      [player, player2, player2]
+    ];
   }]);
